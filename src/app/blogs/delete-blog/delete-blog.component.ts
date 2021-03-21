f6e9d4b77198e97b2bc55fab83ff33831e3ff469
Sidharth Atri <satri@BLR.ALLEGISINDIA.COM>
2021-03-21 11:57:16 +0530

    added delete blog functionality

--- src/app/blogs/delete-blog/delete-blog.component.ts
+++ src/app/blogs/delete-blog/delete-blog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BlogsService } from '../blogs.service';
 
 @Component({
   selector: 'app-delete-blog',
@@ -10,11 +11,15 @@ export class DeleteBlogComponent implements OnInit {
 
   blogId = 0;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private blogsService: BlogsService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(data => {
       this.blogId = data.id;
+
+      this.blogsService.deleteBlog(this.blogId).subscribe(deleteddata=>{
+        console.log("Blog has been Deleted") 
+      })
     });
   }
 }
