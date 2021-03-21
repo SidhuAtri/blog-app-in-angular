577361e5623cb55a8b45e019d8d3d018538c7729
Sidharth Atri <satri@BLR.ALLEGISINDIA.COM>
2021-03-21 19:44:30 +0530

    ui improved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './site-framework/home/home.component';
 
-const routes: Routes = [{ path: 'blogs', loadChildren: () => import('./blogs/blogs.module').then(m => m.BlogsModule) }];
+const routes: Routes = [
+  { path: 'blogs', loadChildren: () => import('./blogs/blogs.module').then(m => m.BlogsModule) },
+  { path: '', component: HomeComponent },
+];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/site-framework/site-framework.module.ts
+++ src/app/site-framework/site-framework.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, SidebarComponent, FooterComponent],
+  declarations: [HeaderComponent, SidebarComponent, FooterComponent, HomeComponent],
   imports: [
     CommonModule
   ],
